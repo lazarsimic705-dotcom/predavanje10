06fc9f42724256d5aa896e0a2a7fcfab79ea48fd
Lenovo <lazarsimic705@gmail.com>
2026-07-27 21:44:29 +0200

    * Renamed data.json to user.json * Added product.json without any data in main.py

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 
-import json  #importovanje data.json u main
+import json  #importovanje user.json u main
 
 
-with open("data.json", "r") as file:  #ucitaj data.json, R-citanje fajla
+with open("data/user.json", "r") as file:  #ucitaj user.json, R-citanje fajla
     data = json.load(file)           # ucitaj podatke iz json fajla
     data.append({
         "nama": "Lazar Nikolic",
@@ -15,7 +15,7 @@ with open("data.json", "r") as file:  #ucitaj data.json, R-citanje fajla
 print(data)
 
 # W - write,  R - Read
-with open("data.json", "w") as file:
+with open("data/user.json", "w") as file:
     json.dump(data, file, indent=4)        # IDENT=4 - lepse pisanje i odvajanje fafla od fajla
 
 
