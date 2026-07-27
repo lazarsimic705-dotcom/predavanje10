a2f614bcdd07d82aeb1fab235832de17ec76b8eb
Lenovo <lazarsimic705@gmail.com>
2026-07-27 21:40:40 +0200

    * Finished main python and data.json

--- main.py
+++ main.py
@@ -1,6 +1,22 @@
 
-# test
+import json  #importovanje data.json u main
 
+
+with open("data.json", "r") as file:  #ucitaj data.json, R-citanje fajla
+    data = json.load(file)           # ucitaj podatke iz json fajla
+    data.append({
+        "nama": "Lazar Nikolic",
+        "age": 22,
+        "height": 195,
+        "gander": "male"
+    })
+
+
+print(data)
+
+# W - write,  R - Read
+with open("data.json", "w") as file:
+    json.dump(data, file, indent=4)        # IDENT=4 - lepse pisanje i odvajanje fafla od fajla
 
 
 
